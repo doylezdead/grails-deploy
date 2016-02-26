4e38334a00ebf4de9b051b067d4f963e8bfcf560
Ryan Doyle <rcdoyle@mtu.edu>
2016-02-25 23:42:52 -0500

    ssh-agent invokation bugfix for git<2

--- resource/build.bash
+++ resource/build.bash
@@ -1,6 +1,7 @@
 #!/bin/bash
-ssh-agent $(ssh-add $1; git clone git@github.com:/$2.git)
+ssh-agent bash -c "ssh-add $1; git clone git@github.com:/$2.git"
+pwd
 cd $3
-grails clean-all
-grails war
+grails clean-all --stacktrace
+grails war --stacktrace
 mv target/*.war ..
