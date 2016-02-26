8c5e40e9fcea47e62e57384749b6ce0bd272cc5c
Ryan Doyle <rcdoyle@mtu.edu>
2016-02-26 00:27:10 -0500

    leftover st

--- resource/build.bash
+++ resource/build.bash
@@ -2,8 +2,8 @@
 ssh-agent bash -c "ssh-add $1; git clone git@github.com:/$2.git"
 pwd
 cd $3
-grails clean-all --stacktrace
-grails war --stacktrace
+grails clean-all
+grails war
 mv target/*.war ..
 
 exit
