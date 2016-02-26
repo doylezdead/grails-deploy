c3740f381b243595420d8ff9d5846ad1ff6da443
Ryan Doyle <rcdoyle@mtu.edu>
2016-02-26 11:51:01 -0500

    debugged?

--- resource/replace.bash
+++ resource/replace.bash
@@ -1,13 +1,19 @@
 #!/bin/bash
 cd $1/webapps
 rm $2
+echo $3
 
 
 while [ `ls | grep $3 | wc -l` -gt 0 ]; do
-    sleep .5;
+    continue
 done
 
 sleep 2;
 chmod 644 ../$2
 mv ../$2 .
+
+while [ `ls | grep $3 | wc -l` -lt 1 ]; do
+    continue
+done
+
 exit
--- resource/upload.bash
+++ resource/upload.bash
@@ -11,7 +11,8 @@ scp -i $1 resource/replace.bash $2@$3:$4
 
 scp -i $1 $5 $2@$3:$4
 
-echo "Deploying war to tomcat"
+echo "Deploying war to tomcat..."
+echo "This may take a while... be patient"
 ssh $2@$3 -i $1 "cd $4; bash replace.bash $4 $5 $6"
-
+echo "Done!"
 exit
